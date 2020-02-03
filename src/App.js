a108dd74049003f86ce669fbdb9ac1bc9ed62e9e
Carla Geglio <carla.geglio@gmail.com>
2020-02-03 13:49:22 -0700

    Add listing images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
+class App extends Component {
+  constructor() {
+    super()
+  }
+
+  render () {
+    return "hi"
+  }
+}
 
 export default App;
