f128b12d5d807cc7d992ba98b6fff7db141951c3
Carla Geglio <carla.geglio@gmail.com>
2020-02-03 18:38:20 -0700

    Add form inputs

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class Form extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {name:'', email:'', purpose:''};
+  }
+
+  handleChange = e => {
+
   }
 
   render() {
@@ -11,8 +15,30 @@ class Form extends Component {
       <form>
         <input
           type="text"
-
+          placeholder="Name..."
+          name="name"
+          value={this.state.name}
+          onChange={this.handleChange}
         />
+        <input
+          type="text"
+          placeholder="Email..."
+          name="email"
+          value={this.state.email}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Purpose..."
+          name="purpose"
+          value={this.state.purpose}
+          onChange={this.handleChange}
+          list="purpose"/>
+        <datalist id="purpose">
+          <option value="Vacation"/>
+          <option value="Business"/>
+          <option value="Other"/>
+        </datalist>
       </form>
     )
   }
