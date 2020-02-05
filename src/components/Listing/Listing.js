39158b1cbc3e541360f68b1a39b74dc024e0a996
Carla Geglio <carla.geglio@gmail.com>
2020-02-04 19:27:08 -0700

    Add images to listings

--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -4,11 +4,12 @@ import './Listing.scss';
 const Listing = (props) => {
   return (
     <article>
+      <img src={process.env.PUBLIC_URL + `/images/${props.listing.listing_id}_a.jpg`} alt="Listing" />
       <h2>{props.listing.name}</h2>
-      <h3>{props.listing.address.street}</h3>
+      <button>Details</button>
     </article>
   )
-}
 
+}
 
 export default Listing;
