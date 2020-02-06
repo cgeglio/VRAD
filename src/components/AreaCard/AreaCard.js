67b868398a63fc5308d17cb5c8cff312400216af
Carla Geglio <carla.geglio@gmail.com>
2020-02-05 18:58:51 -0700

    Add area card and listing container proptypes

--- src/components/AreaCard/AreaCard.js
+++ src/components/AreaCard/AreaCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './AreaCard.scss';
 
 export const AreaCard = ({ nickname, longname, description }) => {
@@ -11,3 +12,9 @@ export const AreaCard = ({ nickname, longname, description }) => {
   </div>
   )
 }
+
+AreaCard.propTypes = {
+  nickname: PropTypes.string,
+  longname: PropTypes.string,
+  description: PropTypes.string
+};
--- src/components/ListingContainer/ListingContainer.js
+++ src/components/ListingContainer/ListingContainer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './ListingContainer.scss';
 import Listing from '../Listing/Listing'
 
-const ListingContainer = (props) => {
-  let listings = props.areas.reduce((acc, area) => {
+const ListingContainer = ({ areas }) => {
+  let listings = areas.reduce((acc, area) => {
     area.listings.forEach(listing => acc.push(listing))
     return acc;
   },[]);
@@ -22,8 +23,8 @@ const ListingContainer = (props) => {
   )
 }
 
-
-
-
-
 export default ListingContainer
+
+ListingContainer.propTypes = {
+  areas: PropTypes.array
+};
