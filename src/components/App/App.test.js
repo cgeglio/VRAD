b1d54fbe4ca97ef04db9a5f4c74d4a624e6c0ae0
Carla Geglio <carla.geglio@gmail.com>
2020-02-05 19:39:53 -0700

    Add snapshot tests for app, excluding fetch testing

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,50 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+  it('should start off with a default state', () => {
+    const expected = {
+      isLoggedIn: false,
+      areas: '',
+      error: '',
+      user: {name: '', email: '', purpose: ''},
+      isLoading: true
+    };
+    expect(wrapper.state()).toEqual(expected);
+  });
+
+  it('should set state with user info when addUser is called', () => {
+    const mockUser = {name: 'Jeff', email: 'jeff@turing.org', purpose: 'vacation'};
+
+    wrapper.instance().addUser(mockUser);
+    expect(wrapper.state()).toEqual({
+      isLoggedIn: true,
+      areas: '',
+      error: '',
+      user: {name: 'Jeff', email: 'jeff@turing.org', purpose: 'vacation'},
+      isLoading: true
+    });
+  });
+
+  it('should reset user info in state when logout is called', () => {
+      const mockUser = {name: 'Jeff', email: 'jeff@turing.org', purpose: 'vacation'};
+
+      wrapper.instance().addUser(mockUser);
+      wrapper.instance().logout();
+      expect(wrapper.state()).toEqual({
+        isLoggedIn: false,
+        areas: '',
+        error: '',
+        user: { name: '', email: '', purpose: ''},
+        isLoading: true
+      });
+  });
 });
