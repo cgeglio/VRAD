4f41af414fa9edf705124eeb4ee3a747092f189b
Andrew Kinstler <andrewckinstler@gmail.com>
2020-02-06 16:47:09 -0700

    styles details page

--- src/components/ListingDetails/ListingDetails.js
+++ src/components/ListingDetails/ListingDetails.js
@@ -8,7 +8,7 @@ const ListingDetails = ({name, listing_id, address, details }) => {
   const createList = details.features.map(feature => <li>{feature}</li>)
   
   return (
-    <article>
+    <article className='listing'>
       <Carousel className='carousel' width='700px'>
         <div>
           <img className='listing-img' src={process.env.PUBLIC_URL + `/images/${listing_id}_a.jpg`} />
@@ -21,22 +21,22 @@ const ListingDetails = ({name, listing_id, address, details }) => {
         </div>
       </Carousel>
 
-      <div className='title'>
-        <h2>{name}</h2>
-        <p>{address.street}, {address.zip}</p>
-      </div>
-      <div className='cost'>
-        <p>${details.cost_per_night} per night</p>
-      </div>
-      <div className='bed-bath'>
-        <p>Beds: {details.beds}</p>
-        <p>Baths: {details.baths}</p>
-      </div>
-      <div className='features'>
-        <h3>Features:</h3>
-        <ul>{createList}</ul>
-      </div>
-
+      <section className='listing-details'>
+        <div className='title'>
+          <h2>{name}</h2>
+          <p>{address.street}, {address.zip}</p>
+        </div>
+        <div>
+          <p className='cost'>${details.cost_per_night} per night</p>
+        </div>
+        <div className='bed-bath'>
+          <p>Beds: {details.beds} / Baths: {details.baths}</p>
+        </div>
+        <div className='features'>
+          <h3>Features:</h3>
+          <ul>{createList}</ul>
+        </div>
+      </section>
     </article>
   )
 }
