7e217e9c74b02ca650837d2fa92eaa2836a2149e
Carla Geglio <carla.geglio@gmail.com>
2020-02-06 17:04:12 -0700

    Change listing preview back to functional component

--- src/components/ListingPreview/ListingPreview.js
+++ src/components/ListingPreview/ListingPreview.js
@@ -1,37 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './ListingPreview.scss';
 import { Link } from 'react-router-dom';
 
 
-class ListingPreview extends Component {
-  constructor() {
-    super();
-  }
-
-  setFavorite = (listing) => {
-    this.props.addFavorite(listing);
-  }
-
-  getImageName = () => {
-    return this.props.listing.favorite ? 'filled' : 'unfilled';
-  }
-
-  render () {
-    const icon = this.getImageName();
-    return (
-      <article>
-        <img src={process.env.PUBLIC_URL + `/images/${this.props.listing.listing_id}_a.jpg`} alt="Listing" className="preview-img" />
-        <div className="listing-preview">
-          <h2 className="listing-name">{this.props.listing.name}</h2>
-          <div className="buttons">
-            <Link to={`/area/${this.props.listing.area_id}/listings/${this.props.listing.listing_id}`}><button onClick={() => {this.props.setCurrentListing(this.props.listing)}} className="details-btn">View Details</button></Link>
-            <button onClick={() => {this.setFavorite(this.props.listing)}} className="favorite-btn"><img src={process.env.PUBLIC_URL + `/${icon}-heart.png`} alt="Favorite icon" className="favorite-img"/></button>
-          </div>
+const ListingPreview = ( { listing, addFavorite, setCurrentListing } ) => {
+  const icon = listing.favorite ? 'filled' : 'unfilled';
+  return (
+    <article>
+      <img src={process.env.PUBLIC_URL + `/images/${listing.listing_id}_a.jpg`} alt="Listing" className="preview-img" />
+      <div className="listing-preview">
+        <h2 className="listing-name">{listing.name}</h2>
+        <div className="buttons">
+          <Link to={`/area/${listing.area_id}/listings/${listing.listing_id}`}><button onClick={() => {setCurrentListing(listing)}} className="details-btn">View Details</button></Link>
+          <button onClick={() => {addFavorite(listing)}} className="favorite-btn"><img src={process.env.PUBLIC_URL + `/${icon}-heart.png`} alt="Favorite icon" className="favorite-img"/></button>
         </div>
-      </article>
-    )
-  }
+      </div>
+    </article>
+  )
 }
 
 export default ListingPreview;
